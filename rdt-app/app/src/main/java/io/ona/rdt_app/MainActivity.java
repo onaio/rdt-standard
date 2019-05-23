2e3299d39299f6374f65d793c7ff087e6e0b8600
Vincent Karuri <vincentmkaruri@gmail.com>
2019-05-23 13:33:18 +0300

    Add button to launch ImageQuality activity

--- rdt-app/app/src/main/java/io/ona/rdt_app/MainActivity.java
+++ rdt-app/app/src/main/java/io/ona/rdt_app/MainActivity.java
@@ -1,7 +1,11 @@
 package io.ona.rdt_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import edu.washington.cs.ubicomplab.rdt_reader.ImageQualityActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_launch_rdt_reader).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImageQualityActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
