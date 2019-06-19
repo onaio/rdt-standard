530db5bd51934bfcd67abfd55f016597bfe574fa
Vincent Karuri <vincentmkaruri@gmail.com>
2019-06-19 16:07:21 +0300

    Write RDT id to rdt widget jsonobject

--- rdt-app/app/src/main/java/io/ona/rdt_app/widget/RDTBarcodeFactory.java
+++ rdt-app/app/src/main/java/io/ona/rdt_app/widget/RDTBarcodeFactory.java
@@ -3,23 +3,74 @@ package io.ona.rdt_app.widget;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.vision.barcode.Barcode;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.fragments.JsonFormFragment;
+import com.vijay.jsonwizard.interfaces.CommonListener;
 import com.vijay.jsonwizard.interfaces.JsonApi;
 import com.vijay.jsonwizard.interfaces.OnActivityResultListener;
 import com.vijay.jsonwizard.widgets.BarcodeFactory;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 import static android.app.Activity.RESULT_OK;
+import static com.vijay.jsonwizard.constants.JsonFormConstants.VALUE;
 
 /**
  * Created by Vincent Karuri on 19/06/2019
  */
 public class RDTBarcodeFactory extends BarcodeFactory {
 
+    private RelativeLayout rootLayout;
+    private JSONObject jsonObject;
+
+    private static final String TAG = RDTBarcodeFactory.class.getName();
+
+    @Override
+    public List<View> getViewsFromJson(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, CommonListener listener) throws Exception {
+        this.jsonObject = jsonObject;
+
+        List<View> views = super.getViewsFromJson(stepName, context, formFragment, jsonObject, listener);
+
+        this.rootLayout = (RelativeLayout) views.get(0);
+
+        hideAndClickScanButton();
+
+        return views;
+    }
+
+    @Override
+    public List<View> getViewsFromJson(String stepName, final Context context,
+                                       JsonFormFragment formFragment, final JSONObject jsonObject,
+                                       CommonListener listener, boolean popup) {
+        this.jsonObject = jsonObject;
+
+        List<View> views = super.getViewsFromJson(stepName, context, formFragment, jsonObject, listener, popup);
+
+        rootLayout = (RelativeLayout) views.get(0);
+
+        hideAndClickScanButton();
+
+        return views;
+    }
+
+    private void hideAndClickScanButton() {
+        Button scanButton = rootLayout.findViewById(com.vijay.jsonwizard.R.id.scan_button);
+        scanButton.setVisibility(View.GONE);
+        scanButton.performClick();
+    }
+
     @Override
     protected void addOnBarCodeResultListeners(final Context context, final MaterialEditText editText) {
+        editText.setVisibility(View.GONE);
         if (context instanceof JsonApi) {
             JsonApi jsonApi = (JsonApi) context;
             jsonApi.addOnActivityResultListener(JsonFormConstants.BARCODE_CONSTANTS.BARCODE_REQUEST_CODE,
@@ -29,11 +80,16 @@ public class RDTBarcodeFactory extends BarcodeFactory {
                                                      int resultCode, Intent data) {
                             if (requestCode == JsonFormConstants.BARCODE_CONSTANTS.BARCODE_REQUEST_CODE && resultCode == RESULT_OK) {
                                 if (data != null) {
-                                    Barcode barcode = data.getParcelableExtra(JsonFormConstants.BARCODE_CONSTANTS.BARCODE_KEY);
-                                    Log.d("Scanned QR Code", barcode.displayValue);
-                                    editText.setText(barcode.displayValue.split(",")[0]);
-                                } else
-                                    Log.i("", "NO RESULT FOR QR CODE");
+                                    try {
+                                        Barcode barcode = data.getParcelableExtra(JsonFormConstants.BARCODE_CONSTANTS.BARCODE_KEY);
+                                        Log.d("Scanned QR Code", barcode.displayValue);
+                                        jsonObject.put(VALUE, barcode.displayValue.split(",")[0]);
+                                    } catch (JSONException e) {
+                                        Log.e(TAG, e.getStackTrace().toString());
+                                    }
+                                } else {
+                                    Log.i("", "No result for qr code");
+                                }
                             }
                         }
                     });
