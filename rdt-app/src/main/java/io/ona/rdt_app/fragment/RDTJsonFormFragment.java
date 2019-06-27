d20f52bf3e08badbbe7304a5825cfce6562dd99d
Vincent Karuri <vincentmkaruri@gmail.com>
2019-06-27 12:21:14 +0300

    Move files into new folder structure

--- rdt-app/src/main/java/io/ona/rdt_app/fragment/RDTJsonFormFragment.java
+++ rdt-app/src/main/java/io/ona/rdt_app/fragment/RDTJsonFormFragment.java
@@ -12,7 +12,7 @@ import org.json.JSONObject;
 
 import io.ona.rdt_app.presenter.RDTJsonFormFragmentPresenter;
 import io.ona.rdt_app.interactor.RDTJsonFormInteractor;
-import util.RDTCaptureJsonFormUtils;
+import io.ona.rdt_app.util.RDTCaptureJsonFormUtils;
 
 import static io.ona.rdt_app.util.Constants.REQUEST_CODE_GET_JSON;
 
