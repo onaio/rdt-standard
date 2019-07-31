09075f4d093ae759beb6d09662d385039369ef52
Vincent Karuri <vincentmkaruri@gmail.com>
2019-07-31 15:35:21 +0300

    Add barcode factory test

--- rdt-app/src/test/java/io/ona/rdt_app/widget/RDTBarcodeFactoryTest.java
+++ rdt-app/src/test/java/io/ona/rdt_app/widget/RDTBarcodeFactoryTest.java
@@ -5,10 +5,13 @@ import org.junit.Test;
 import org.powermock.reflect.Whitebox;
 
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Vincent Karuri on 31/07/2019
@@ -41,4 +44,27 @@ public class RDTBarcodeFactoryTest {
         Date result = Whitebox.invokeMethod(barcodeFactory, "convertDate", dateStr);
         assertNull(result);
     }
+
+    @Test
+    public void testConvertDateShouldReturnCorrectDateForValidDateFormat() throws Exception {
+        String dateStr = "201217";
+        Date result = Whitebox.invokeMethod(barcodeFactory, "convertDate", dateStr);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(result);
+        assertEquals(calendar.get(Calendar.DATE), 20);
+        assertEquals(calendar.get(Calendar.MONTH), 11); // month is 0-indexed
+        assertEquals(calendar.get(Calendar.YEAR), 2017);
+    }
+
+//    @Test
+//    public void testIsRDTExpiredShouldReturnTrueForExpiredRDT() throws Exception {
+//        boolean result = Whitebox.invokeMethod(barcodeFactory,  "isRDTExpired", new Date(1999, 12, 9));
+//        assertTrue(result);
+//    }
+//
+//    @Test
+//    public void testIsRDTExpiredShouldReturnFalseForValidRDT() throws Exception {
+//        boolean result = Whitebox.invokeMethod(barcodeFactory, "isRDTExpired", new Date());
+//        assertFalse(result);
+//    }
 }
