18d9b036cc0b6725b9d8b54545115ad3f9ecdbb2
Vincent Karuri <vincentmkaruri@gmail.com>
2019-07-31 12:32:27 +0300

    add barcode factory test

--- rdt-app/src/test/java/io/ona/rdt_app/widget/RDTBarcodeFactoryTest.java
+++ rdt-app/src/test/java/io/ona/rdt_app/widget/RDTBarcodeFactoryTest.java
@@ -1,7 +1,35 @@
 package io.ona.rdt_app.widget;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Vincent Karuri on 31/07/2019
  */
 public class RDTBarcodeFactoryTest {
+
+    private RDTBarcodeFactory barcodeFactory;
+
+    @Before
+    public void setUp() {
+        barcodeFactory = new RDTBarcodeFactory();
+    }
+
+    @Test
+    public void testGetDateStrShouldReturnEmptyStringForNullDate() throws Exception {
+        String result = Whitebox.invokeMethod(barcodeFactory, "getDateStr", null);
+        assertEquals(result, "");
+    }
+
+    @Test
+    public void testGetDateStrShouldReturnDateStringForNonNullDate() throws Exception {
+        Date now = new Date();
+        String result = Whitebox.invokeMethod(barcodeFactory, "getDateStr", now);
+        assertEquals(result, now.toString());
+    }
 }
