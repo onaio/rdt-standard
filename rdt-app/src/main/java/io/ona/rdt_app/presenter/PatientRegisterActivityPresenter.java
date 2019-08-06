f3141e86b84fac459e3ac195d077dae26bd42035
Vincent Karuri <vincentmkaruri@gmail.com>
2019-08-06 17:09:53 +0300

    Populate entity id if blank

--- rdt-app/src/main/java/io/ona/rdt_app/presenter/PatientRegisterActivityPresenter.java
+++ rdt-app/src/main/java/io/ona/rdt_app/presenter/PatientRegisterActivityPresenter.java
@@ -2,18 +2,22 @@ package io.ona.rdt_app.presenter;
 
 import android.app.Activity;
 
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.view.contract.BaseRegisterContract;
 
 import java.util.List;
+import java.util.UUID;
 
 import io.ona.rdt_app.callback.OnFormSavedCallback;
 import io.ona.rdt_app.contract.PatientRegisterActivityContract;
 import io.ona.rdt_app.interactor.PatientRegisterActivityInteractor;
 import io.ona.rdt_app.model.Patient;
 
+import static io.ona.rdt_app.util.Constants.ENTITY_ID;
 import static io.ona.rdt_app.util.Constants.Form.RDT_TEST_FORM;
+import static org.smartregister.util.JsonFormUtils.getString;
 
 /**
  * Created by Vincent Karuri on 07/06/2019
@@ -53,6 +57,11 @@ public class PatientRegisterActivityPresenter implements BaseRegisterContract.Pr
     @Override
     public void saveForm(String jsonForm, OnFormSavedCallback callback) throws JSONException {
         JSONObject jsonFormObj = new JSONObject(jsonForm);
+        String entityId = getString(jsonForm, ENTITY_ID);
+        if (StringUtils.isBlank(entityId)) {
+            jsonFormObj.put(ENTITY_ID, UUID.randomUUID().toString());
+        }
+
         interactor.saveForm(jsonFormObj, callback);
         Patient patient = interactor.getPatientForRDT(jsonFormObj);
         if (patient != null) {
