671a888e6778815efb126526a68e4676c3beb51d
Vincent Karuri <vincentmkaruri@gmail.com>
2019-08-15 21:35:35 +0300

    Fix failing tests

--- rdt-app/src/test/java/io/ona/rdt_app/presenter/RDTJsonFormFragmentPresenterTest.java
+++ rdt-app/src/test/java/io/ona/rdt_app/presenter/RDTJsonFormFragmentPresenterTest.java
@@ -45,7 +45,7 @@ public class RDTJsonFormFragmentPresenterTest {
     @Test
     public void testPerformNextButtonActionShouldNavigateToNextStepAndSaveFormFromExpirationPage() {
         presenter.attachView((JsonFormFragment) fragment);
-        presenter.performNextButtonAction("5", null);
+        presenter.performNextButtonAction("step5", null);
         verify(interactor).saveForm(any(JSONObject.class));
         verify(fragment).moveToNextStep();
     }
@@ -57,14 +57,14 @@ public class RDTJsonFormFragmentPresenterTest {
 
         JsonFormFragment formFragment = mock(JsonFormFragment.class);
         when(RDTJsonFormFragment.getFormFragment(anyString())).thenReturn(formFragment);
-        presenter.performNextButtonAction("8", null);
+        presenter.performNextButtonAction("step8", null);
         verify(fragment).transactFragment(eq(formFragment));
     }
 
     @Test
     public void testPerformNextButtonActionShouldShowImageViewsForONARDT() {
         doReturn(Constants.ONA_RDT).when(fragment).getRDTType();
-        presenter.performNextButtonAction("8", null);
+        presenter.performNextButtonAction("step8", null);
         verify(fragment).moveToNextStep();
     }
 
