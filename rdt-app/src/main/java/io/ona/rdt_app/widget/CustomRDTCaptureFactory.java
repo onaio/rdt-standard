403a1350923b4b7d091d33c84f49d58d762b0492
Vincent Karuri <vincentmkaruri@gmail.com>
2019-10-11 12:29:49 +0300

    Fix logical bug

--- rdt-app/src/main/java/io/ona/rdt_app/widget/CustomRDTCaptureFactory.java
+++ rdt-app/src/main/java/io/ona/rdt_app/widget/CustomRDTCaptureFactory.java
@@ -130,7 +130,7 @@ public class CustomRDTCaptureFactory extends RDTCaptureFactory {
                         interpretationResult.bottomLine = Boolean.valueOf(pfResult);
 
                         populateRelevantFields(imgIDAndTimeStamp, imgIdAddress, imgTimeStampAddress, interpretationResult, imageCaptureDuration, (JsonApi) widgetArgs.getContext());
-                        if (formFragment.getFragmentPresenter().hasNextStep()) {
+                        if (!formFragment.getFragmentPresenter().hasNextStep()) {
                             formFragment.save(true);
                         }
                     } catch (JSONException e) {
