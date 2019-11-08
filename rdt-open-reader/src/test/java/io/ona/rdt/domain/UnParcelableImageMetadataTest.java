781eec5675146a09cc7be55875f5aebad75ac620
Vincent Karuri <vincentmkaruri@gmail.com>
2019-11-08 16:34:01 +0300

    Add UnParcelableImageMetadata tests

---
+++ rdt-open-reader/src/test/java/io/ona/rdt/domain/UnParcelableImageMetadataTest.java
@@ -0,0 +1,28 @@
+package io.ona.rdt.domain;
+
+import org.junit.Test;
+import org.opencv.core.Point;
+
+import edu.washington.cs.ubicomplab.rdt_reader.ImageProcessor;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by Vincent Karuri on 08/11/2019
+ */
+public class UnParcelableImageMetadataTest {
+
+    @Test
+    public void testUnParcelableImageMetadataBuilder() {
+        Point[] points = new Point[]{};
+        ImageProcessor.InterpretationResult interpretationResult = mock(ImageProcessor.InterpretationResult.class);
+
+        UnParcelableImageMetadata unParcelableImageMetadata = new UnParcelableImageMetadata();
+        unParcelableImageMetadata.withInterpretationResult(interpretationResult);
+        unParcelableImageMetadata.withBoundary(points);
+
+        assertEquals(interpretationResult, unParcelableImageMetadata.getInterpretationResult());
+        assertEquals(points, unParcelableImageMetadata.getBoundary());
+    }
+}
