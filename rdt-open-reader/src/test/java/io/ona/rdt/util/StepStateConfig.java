52620563b8c406a7dcd3c5bc0b0a4a9a4b4318bc
Vincent Karuri <vincentmkaruri@gmail.com>
2019-11-29 13:19:00 +0300

    Add step config class and json file

---
+++ rdt-open-reader/src/test/java/io/ona/rdt/util/StepStateConfig.java
@@ -0,0 +1,39 @@
+package io.ona.rdt.util;
+
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Vincent Karuri on 29/11/2019
+ */
+public class StepStateConfig {
+
+    private static final String STEP_STATE_CONFIG_FILE = "step-config/step-state-config.json";
+    private static StepStateConfig stepStateConfig;
+    private static JSONObject stepStateObj;
+
+    private StepStateConfig() { }
+
+    public static StepStateConfig getInstance(Context context) throws JSONException {
+        if (stepStateConfig == null) {
+            stepStateConfig = new StepStateConfig();
+            JSONObject configJSONObj = new RDTJsonFormUtils().getFormJsonObject(STEP_STATE_CONFIG_FILE, context);
+            stepStateConfig.setStepStateObj(configJSONObj);
+        }
+        return stepStateConfig;
+    }
+
+    public void destroyInstance() {
+        stepStateConfig = null;
+    }
+
+    public void setStepStateObj(JSONObject stepStateObj) {
+        StepStateConfig.stepStateObj = stepStateObj;
+    }
+
+    public static JSONObject getStepStateObj() {
+        return stepStateObj;
+    }
+}
