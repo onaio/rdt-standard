3780762fdb7495d8b93fd5fd7b51b30067140255
Vincent Karuri <vincentmkaruri@gmail.com>
2019-12-02 17:20:44 +0300

    Fix failing RDTJsonFormFragmentPresenter tests

--- rdt-open-reader/src/test/java/io/ona/rdt/presenter/RDTJsonFormFragmentPresenterTest.java
+++ rdt-open-reader/src/test/java/io/ona/rdt/presenter/RDTJsonFormFragmentPresenterTest.java
@@ -5,22 +5,35 @@ import com.vijay.jsonwizard.presenters.JsonFormFragmentPresenter;
 import com.vijay.jsonwizard.views.JsonFormFragmentView;
 import com.vijay.jsonwizard.viewstates.JsonFormFragmentViewState;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.AdditionalMatchers;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
 import java.lang.ref.WeakReference;
 
+import io.ona.rdt.application.RDTApplication;
 import io.ona.rdt.contract.RDTJsonFormFragmentContract;
 import io.ona.rdt.fragment.RDTJsonFormFragment;
 import io.ona.rdt.interactor.RDTJsonFormInteractor;
 import io.ona.rdt.util.Constants;
+import io.ona.rdt.util.StepStateConfig;
 
+import static io.ona.rdt.util.Constants.Step.BLOT_PAPER_TASK_PAGE;
+import static io.ona.rdt.util.Constants.Step.EXPIRATION_DATE_READER_ADDRESS;
+import static io.ona.rdt.util.Constants.Step.RDT_EXPIRED_PAGE;
+import static io.ona.rdt.util.Constants.Step.RDT_ID_KEY;
+import static io.ona.rdt.util.Constants.Step.RDT_ID_LBL_ADDRESSES;
+import static io.ona.rdt.util.Constants.Step.SCAN_CARESTART_PAGE;
+import static io.ona.rdt.util.Constants.Step.SCAN_QR_PAGE;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -38,7 +51,7 @@ import static org.powermock.api.support.membermodification.MemberModifier.suppre
  * Created by Vincent Karuri on 14/08/2019
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({RDTJsonFormFragment.class, RDTJsonFormFragmentPresenter.class})
+@PrepareForTest({RDTJsonFormFragment.class, RDTJsonFormFragmentPresenter.class, RDTApplication.class})
 public class RDTJsonFormFragmentPresenterTest {
 
     private RDTJsonFormFragmentPresenter presenter;
@@ -47,11 +60,18 @@ public class RDTJsonFormFragmentPresenterTest {
     private JsonFormFragment formFragment;
     private JsonFormFragmentView<JsonFormFragmentViewState> view;
 
+    @Mock
+    private RDTApplication rdtApplication;
+
+    @Mock
+    private StepStateConfig stepStateConfig;
+
     @Before
-    public void setUp() {
+    public void setUp() throws JSONException {
         interactor = mock(RDTJsonFormInteractor.class);
         rdtFormfragment = spy(new PatientRegisterFragmentStub());
         presenter = new RDTJsonFormFragmentPresenter(rdtFormfragment, interactor);
+        mockStaticMethods();
     }
 
     @Test
@@ -146,5 +166,28 @@ public class RDTJsonFormFragmentPresenterTest {
         mockStatic(RDTJsonFormFragment.class);
         formFragment = mock(JsonFormFragment.class);
         when(RDTJsonFormFragment.getFormFragment(anyString())).thenReturn(formFragment);
+    }
+
+    private void mockStaticMethods() throws JSONException {
+        // mock RDTApplication and Drishti context
+        mockStatic(RDTApplication.class);
+        PowerMockito.when(RDTApplication.getInstance()).thenReturn(rdtApplication);
+        PowerMockito.when(rdtApplication.getStepStateConfiguration()).thenReturn(stepStateConfig);
+
+        JSONObject jsonObject = mock(JSONObject.class);
+        doReturn("step1").when(jsonObject).optString(AdditionalMatchers.or(eq(SCAN_CARESTART_PAGE), eq(SCAN_QR_PAGE)));
+        doReturn("step1").when(jsonObject).optString(eq(RDT_EXPIRED_PAGE), anyString());
+        doReturn("step6").when(jsonObject).optString(eq(RDT_EXPIRED_PAGE));
+        doReturn("step9").when(jsonObject).optString(eq(BLOT_PAPER_TASK_PAGE));
+        doReturn("step1:expiration_date_reader").when(jsonObject).optString(eq(EXPIRATION_DATE_READER_ADDRESS), anyString());
+        doReturn("rdt_id").when(jsonObject).optString(eq(RDT_ID_KEY));
+        doReturn(new JSONArray("[\n" +
+                "    \"step7:lbl_rdt_id\",\n" +
+                "    \"step8:lbl_rdt_id\",\n" +
+                "    \"step9:lbl_rdt_id\",\n" +
+                "    \"step18:lbl_rdt_id\",\n" +
+                "    \"step19:lbl_rdt_id\"\n" +
+                "  ]")).when(jsonObject).optJSONArray(eq(RDT_ID_LBL_ADDRESSES));
+        doReturn(jsonObject).when(stepStateConfig).getStepStateObj();
     }
 }
