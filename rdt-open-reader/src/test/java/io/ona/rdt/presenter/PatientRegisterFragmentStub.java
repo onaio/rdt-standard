09280a74fa007d8b879d917f83cd380cb4ed343a
Vincent Karuri <vincentmkaruri@gmail.com>
2020-01-31 14:11:22 +0300

    Fix failing tests

--- rdt-open-reader/src/test/java/io/ona/rdt/presenter/PatientRegisterFragmentStub.java
+++ rdt-open-reader/src/test/java/io/ona/rdt/presenter/PatientRegisterFragmentStub.java
@@ -6,9 +6,9 @@ import com.vijay.jsonwizard.fragments.JsonFormFragment;
 import com.vijay.jsonwizard.interfaces.JsonApi;
 
 import io.ona.rdt.contract.RDTJsonFormFragmentContract;
-import io.ona.rdt.stub.JsonApiStub;
 
 import static io.ona.rdt.interactor.PatientRegisterFragmentInteractorTest.PATIENT_REGISTRATION_JSON_FORM;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Vincent Karuri on 14/08/2019
@@ -42,7 +42,7 @@ public class PatientRegisterFragmentStub extends JsonFormFragment implements RDT
 
     @Override
     public JsonApi getJsonApi() {
-        return new JsonApiStub();
+        return mock(JsonApi.class);
     }
 
     @Override
