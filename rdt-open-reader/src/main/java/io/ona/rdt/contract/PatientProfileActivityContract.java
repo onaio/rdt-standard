7beadd2a85e453f3e9d30bc3cdde5a290bf80eee
Vincent Karuri <vincentmkaruri@gmail.com>
2020-02-17 14:21:03 +0300

    Allow saving of test forms from patient profile

--- rdt-open-reader/src/main/java/io/ona/rdt/contract/PatientProfileActivityContract.java
+++ rdt-open-reader/src/main/java/io/ona/rdt/contract/PatientProfileActivityContract.java
@@ -2,6 +2,8 @@ package io.ona.rdt.contract;
 
 import android.support.v4.app.Fragment;
 
+import io.ona.rdt.callback.OnFormSavedCallback;
+
 /**
  * Created by Vincent Karuri on 29/01/2020
  */
@@ -11,5 +13,7 @@ public interface PatientProfileActivityContract {
         void replaceFragment(Fragment fragment, boolean addToBackStack);
     }
 
-    interface Presenter {}
+    interface Presenter {
+        void saveForm(String jsonForm, OnFormSavedCallback callback);
+    }
 }
