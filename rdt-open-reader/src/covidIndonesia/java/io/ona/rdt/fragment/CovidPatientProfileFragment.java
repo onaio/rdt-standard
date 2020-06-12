d7f2a87eaa7e85983d9ed6d497062ee81d095986
Vincent Karuri <vincentmkaruri@gmail.com>
2020-06-12 18:08:34 +0300

    Add covid patient profile layout

---
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/fragment/CovidPatientProfileFragment.java
@@ -0,0 +1,9 @@
+package io.ona.rdt.fragment;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Vincent Karuri on 12/06/2020
+ */
+public class CovidPatientProfileFragment extends Fragment {
+}
