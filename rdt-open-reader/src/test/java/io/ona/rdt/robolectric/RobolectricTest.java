ee1913e27a12e8f009870a9ca0694fd6831daeb3
Vincent Karuri <vincentmkaruri@gmail.com>
2020-07-24 10:03:50 +0300

    Add PatientRegisterFragment tests

--- rdt-open-reader/src/test/java/io/ona/rdt/robolectric/RobolectricTest.java
+++ rdt-open-reader/src/test/java/io/ona/rdt/robolectric/RobolectricTest.java
@@ -8,16 +8,18 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import io.ona.rdt.robolectric.shadow.BaseLoaderCallbackShadow;
 import io.ona.rdt.robolectric.shadow.ClientCoreUtilsShadow;
 import io.ona.rdt.robolectric.shadow.FirebaseCrashlyticsShadow;
 import io.ona.rdt.robolectric.shadow.FormDataRepositoryShadow;
 import io.ona.rdt.robolectric.shadow.CameraBridgeViewBaseShadow;
+import io.ona.rdt.robolectric.shadow.OpenSRPContextShadow;
 import io.ona.rdt.robolectric.shadow.UtilsShadow;
 
-@RunWith(RobolectricTestRunner.class)
+@RunWith(AndroidJUnit4.class)
 @Config(shadows = {ClientCoreUtilsShadow.class, FirebaseCrashlyticsShadow.class,
         FormDataRepositoryShadow.class, UtilsShadow.class, CameraBridgeViewBaseShadow.class,
-        BaseLoaderCallbackShadow.class})
+        BaseLoaderCallbackShadow.class, OpenSRPContextShadow.class})
 public abstract class  RobolectricTest {
 }
