bc59081c26c9f2bef893071775fe132af40e3925
Vincent Karuri <vincentmkaruri@gmail.com>
2020-07-28 13:16:49 +0300

    Add TestsProfileFragment tests

--- rdt-open-reader/src/test/java/io/ona/rdt/robolectric/fragment/TestsProfileFragmentTest.java
+++ rdt-open-reader/src/test/java/io/ona/rdt/robolectric/fragment/TestsProfileFragmentTest.java
@@ -1,36 +1,33 @@
 package io.ona.rdt.robolectric.fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.util.ReflectionHelpers;
-
-import java.util.ArrayList;
 
 import androidx.fragment.app.testing.FragmentScenario;
 import io.ona.rdt.R;
 import io.ona.rdt.domain.FormattedRDTTestDetails;
-import io.ona.rdt.domain.ParasiteProfileResult;
 import io.ona.rdt.fragment.TestsProfileFragment;
-import io.ona.rdt.presenter.TestsProfileFragmentPresenter;
 import io.ona.rdt.robolectric.RobolectricTest;
 
+import static io.ona.rdt.util.Constants.Test.INVALID;
+import static io.ona.rdt.util.Constants.Test.NEGATIVE;
+import static io.ona.rdt.util.Constants.Test.POSITIVE;
 import static io.ona.rdt.util.Constants.Test.RDT_TEST_DETAILS;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Vincent Karuri on 27/07/2020
  */
 public class TestsProfileFragmentTest extends RobolectricTest {
-
-    @Mock
-    private TestsProfileFragmentPresenter presenter;
 
     @Before
     public void setUp() {
@@ -39,19 +36,11 @@ public class TestsProfileFragmentTest extends RobolectricTest {
 
     @Test
     public void testFragmentCreationShouldInitializeFragmentState() {
-        doReturn(new ArrayList<ParasiteProfileResult>()).when(presenter)
-                .getParasiteProfiles(anyString(), anyString(), anyString());
 
-        FormattedRDTTestDetails expectedformattedRDTTestDetails = new FormattedRDTTestDetails();
-        expectedformattedRDTTestDetails.setFormattedRDTId("rdt_id");
-        expectedformattedRDTTestDetails.setFormattedRDTTestDate("11/20/2202");
-        expectedformattedRDTTestDetails.setFormattedRDTType("malaria");
-        expectedformattedRDTTestDetails.setFormattedTestResults("test_results");
-        expectedformattedRDTTestDetails.setTestResult("test_result");
+        FormattedRDTTestDetails expectedformattedRDTTestDetails = getFormattedRDTTestDetails(NEGATIVE, "");
 
         Bundle bundle = new Bundle();
         bundle.putParcelable(RDT_TEST_DETAILS, expectedformattedRDTTestDetails);
-
 
         FragmentScenario<TestsProfileFragment> fragmentScenario =
                 FragmentScenario.launchInContainer(TestsProfileFragment.class,
@@ -62,6 +51,101 @@ public class TestsProfileFragmentTest extends RobolectricTest {
             FormattedRDTTestDetails actualFormattedRDTTestDetails = ReflectionHelpers.getField(fragment,
                     "formattedRDTTestDetails");
             assertEquals(expectedformattedRDTTestDetails, actualFormattedRDTTestDetails);
+
+            View rootLayout = ReflectionHelpers.getField(fragment, "rootLayout");
+            TextView pvResult = rootLayout.findViewById(R.id.tv_rdt_pv_result);
+            TextView pfResult = rootLayout.findViewById(R.id.tv_rdt_pf_result);
+            assertEquals(fragment.getContext().getString(R.string.pv_negative_result), pvResult.getText());
+            assertEquals(fragment.getContext().getString(R.string.pf_negative_result), pfResult.getText());
         });
+    }
+
+    @Test
+    public void testPopulateRDTTestResultsShouldSetInvalidTestResults() {
+        FormattedRDTTestDetails expectedformattedRDTTestDetails = getFormattedRDTTestDetails(INVALID, "");
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(RDT_TEST_DETAILS, expectedformattedRDTTestDetails);
+
+        FragmentScenario<TestsProfileFragment> fragmentScenario =
+                FragmentScenario.launchInContainer(TestsProfileFragment.class,
+                        bundle, R.style.AppTheme, null);
+
+        fragmentScenario.onFragment(fragment -> {
+            View rootLayout = ReflectionHelpers.getField(fragment, "rootLayout");
+            assertEquals(View.GONE, rootLayout.findViewById(R.id.tv_rdt_pv_result).getVisibility());
+            TextView pfResult = rootLayout.findViewById(R.id.tv_rdt_pf_result);
+            assertEquals(fragment.getContext().getString(R.string.invalid_result), pfResult.getText());
+        });
+    }
+
+
+    @Test
+    public void testPopulateRDTTestResultsShouldSetPositiveTestResults() {
+        FormattedRDTTestDetails expectedformattedRDTTestDetails = getFormattedRDTTestDetails(POSITIVE, "positive, positive");
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(RDT_TEST_DETAILS, expectedformattedRDTTestDetails);
+
+        FragmentScenario<TestsProfileFragment> fragmentScenario =
+                FragmentScenario.launchInContainer(TestsProfileFragment.class,
+                        bundle, R.style.AppTheme, null);
+
+        // both pv and pf have results
+        fragmentScenario.onFragment(fragment -> {
+            View rootLayout = ReflectionHelpers.getField(fragment, "rootLayout");
+            TextView pvResult = rootLayout.findViewById(R.id.tv_rdt_pv_result);
+            TextView pfResult = rootLayout.findViewById(R.id.tv_rdt_pf_result);
+            assertEquals("positive", pfResult.getText());
+            assertEquals("positive", pvResult.getText());
+        });
+
+        // pf positive result
+        expectedformattedRDTTestDetails = getFormattedRDTTestDetails(POSITIVE,
+                RuntimeEnvironment.application.getString(R.string.pf_positive_result));
+
+        bundle = new Bundle();
+        bundle.putParcelable(RDT_TEST_DETAILS, expectedformattedRDTTestDetails);
+
+        fragmentScenario =
+                FragmentScenario.launchInContainer(TestsProfileFragment.class,
+                        bundle, R.style.AppTheme, null);
+
+        fragmentScenario.onFragment(fragment -> {
+            View rootLayout = ReflectionHelpers.getField(fragment, "rootLayout");
+            TextView pvResult = rootLayout.findViewById(R.id.tv_rdt_pv_result);
+            TextView pfResult = rootLayout.findViewById(R.id.tv_rdt_pf_result);
+            assertEquals(RuntimeEnvironment.application.getString(R.string.pf_positive_result), pfResult.getText());
+            assertEquals(RuntimeEnvironment.application.getString(R.string.pv_negative_result), pvResult.getText());
+        });
+
+        // pv positive result
+        expectedformattedRDTTestDetails = getFormattedRDTTestDetails(POSITIVE,
+                RuntimeEnvironment.application.getString(R.string.pv_positive_result));
+
+        bundle = new Bundle();
+        bundle.putParcelable(RDT_TEST_DETAILS, expectedformattedRDTTestDetails);
+
+        fragmentScenario =
+                FragmentScenario.launchInContainer(TestsProfileFragment.class,
+                        bundle, R.style.AppTheme, null);
+
+        fragmentScenario.onFragment(fragment -> {
+            View rootLayout = ReflectionHelpers.getField(fragment, "rootLayout");
+            TextView pvResult = rootLayout.findViewById(R.id.tv_rdt_pv_result);
+            TextView pfResult = rootLayout.findViewById(R.id.tv_rdt_pf_result);
+            assertEquals(RuntimeEnvironment.application.getString(R.string.pv_positive_result), pfResult.getText());
+            assertEquals(RuntimeEnvironment.application.getString(R.string.pf_negative_result), pvResult.getText());
+        });
+    }
+
+    private FormattedRDTTestDetails getFormattedRDTTestDetails(String testResult, String formattedTestResults) {
+        FormattedRDTTestDetails expectedformattedRDTTestDetails = new FormattedRDTTestDetails();
+        expectedformattedRDTTestDetails.setFormattedRDTId("rdt_id");
+        expectedformattedRDTTestDetails.setFormattedRDTTestDate("11/20/2202");
+        expectedformattedRDTTestDetails.setFormattedRDTType("malaria");
+        expectedformattedRDTTestDetails.setFormattedTestResults(formattedTestResults);
+        expectedformattedRDTTestDetails.setTestResult(testResult);
+        return expectedformattedRDTTestDetails;
     }
 }
