65b967e5658d5ebf3630b9e80f24de2343ee0713
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-19 15:58:32 +0300

    Add visit data model

---
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/domain/Visit.java
@@ -0,0 +1,26 @@
+package io.ona.rdt.domain;
+
+/**
+ * Created by Vincent Karuri on 19/08/2020
+ */
+public class Visit {
+
+    private String visitName;
+    private String dateOfVisit;
+
+    public String getVisitName() {
+        return visitName;
+    }
+
+    public void setVisitName(String visitName) {
+        this.visitName = visitName;
+    }
+
+    public String getDateOfVisit() {
+        return dateOfVisit;
+    }
+
+    public void setDateOfVisit(String dateOfVisit) {
+        this.dateOfVisit = dateOfVisit;
+    }
+}
