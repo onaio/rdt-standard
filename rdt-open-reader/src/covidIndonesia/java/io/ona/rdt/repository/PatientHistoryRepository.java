4ad01d4be78682533342dcf8220d2af192c86d24
Vincent Karuri <vincentmkaruri@gmail.com>
2020-08-21 16:24:15 +0300

    Add method to get events by event type and date

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/repository/PatientHistoryRepository.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/repository/PatientHistoryRepository.java
@@ -1,6 +1,5 @@
 package io.ona.rdt.repository;
 
-import org.apache.commons.lang3.StringUtils;
 import org.smartregister.domain.db.EventClient;
 import org.smartregister.repository.EventClientRepository;
 
@@ -18,12 +17,27 @@ public class PatientHistoryRepository {
 
     public List<EventClient> getEventsByUniqueDate(String baseEntityId) {
         return eventClientRepository.fetchEventClientsCore(
-                String.format(
-                        "SELECT %s, SUBSTR(%s, %s, %s) visit_date FROM event WHERE %s=? GROUP BY 2",
-                        EventClientRepository.event_column.json.toString(),
-                        EventClientRepository.event_column.dateCreated.toString(), "1", "10",
-                        EventClientRepository.event_column.baseEntityId.toString()
-                ),
-                new String[]{baseEntityId});
+            String.format(
+                    "SELECT %s, SUBSTR(%s, %s, %s) visit_date FROM event WHERE %s=? GROUP BY 2",
+                    EventClientRepository.event_column.json.toString(),
+                    EventClientRepository.event_column.dateCreated.toString(), "1", "10",
+                    EventClientRepository.event_column.baseEntityId.toString()
+            ),
+            new String[]{baseEntityId}
+        );
+    }
+
+    public List<EventClient> getEventsByEventType(String baseEntityId, String eventType, String date) {
+        return eventClientRepository.fetchEventClientsCore(
+            String.format(
+                    "SELECT %s, SUBSTR(%s, %s, %s) AS visit_date FROM event " +
+                            "WHERE %s=? AND %s=? AND visit_date=?",
+                    EventClientRepository.event_column.json.toString(),
+                    EventClientRepository.event_column.dateCreated.toString(), "1", "10",
+                    EventClientRepository.event_column.baseEntityId.toString(),
+                    EventClientRepository.event_column.eventType.toString()
+            ),
+            new String[]{baseEntityId, eventType, date}
+        );
     }
 }
