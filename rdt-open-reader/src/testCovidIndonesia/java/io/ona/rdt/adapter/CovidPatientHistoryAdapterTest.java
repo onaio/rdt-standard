7ec039d3012f0ae3ed233c6f58abeb3afd24fb93
syedowaisali <12345>
2020-08-24 19:48:07 +0500

    :hammer: fixed codacy issues

--- rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/adapter/CovidPatientHistoryAdapterTest.java
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/adapter/CovidPatientHistoryAdapterTest.java
@@ -7,10 +7,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -21,26 +24,19 @@ import io.ona.rdt.R;
 import io.ona.rdt.domain.PatientHistoryEntry;
 import io.ona.rdt.viewholder.CovidPatientHistoryViewHolder;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.mock;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
-
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(LayoutInflater.class)
 public class CovidPatientHistoryAdapterTest {
+
+    private static final String PATIENT_NAME_KEY = "patient_name";
+    private static final String PATIENT_NAME_VAL = "Jane";
 
     private CovidPatientHistoryAdapter adapter;
     private List<PatientHistoryEntry> list = new ArrayList<>();
 
     @Before
     public void setUp() {
-        list.add(new PatientHistoryEntry("patient_name", "Jane"));
+        list.add(new PatientHistoryEntry(PATIENT_NAME_KEY, PATIENT_NAME_VAL));
         adapter = new CovidPatientHistoryAdapter(list);
     }
 
@@ -53,38 +49,38 @@ public class CovidPatientHistoryAdapterTest {
     @Test
     public void onCreateViewHolderShouldReturnNewInstance() {
 
-        mockStatic(LayoutInflater.class);
+        PowerMockito.mockStatic(LayoutInflater.class);
 
-        ViewGroup viewGroup = mock(ViewGroup.class);
-        Context context = mock(Context.class);
-        View view = mock(View.class);
-        LayoutInflater inflater = mock(LayoutInflater.class);
+        ViewGroup viewGroup = PowerMockito.mock(ViewGroup.class);
+        Context context = PowerMockito.mock(Context.class);
+        View view = PowerMockito.mock(View.class);
+        LayoutInflater inflater = PowerMockito.mock(LayoutInflater.class);
 
-        when(viewGroup.getContext()).thenReturn(context);
-        when(LayoutInflater.from(any(Context.class))).thenReturn(inflater);
-        when(inflater.inflate(anyInt(), any(ViewGroup.class), anyBoolean())).thenReturn(view);
+        PowerMockito.when(viewGroup.getContext()).thenReturn(context);
+        PowerMockito.when(LayoutInflater.from(ArgumentMatchers.any(Context.class))).thenReturn(inflater);
+        PowerMockito.when(inflater.inflate(ArgumentMatchers.anyInt(), ArgumentMatchers.any(ViewGroup.class), ArgumentMatchers.anyBoolean())).thenReturn(view);
 
         CovidPatientHistoryViewHolder viewHolder = adapter.onCreateViewHolder(viewGroup, R.layout.covid_patient_history_row);
-        assertNotNull(viewHolder);
+        Assert.assertNotNull(viewHolder);
     }
 
     @Test
     public void onBindViewHolderShouldVerify() {
-        CovidPatientHistoryViewHolder holder = mock(CovidPatientHistoryViewHolder.class);
-        TextView keyTextView = mock(TextView.class);
-        TextView valTextView = mock(TextView.class);
+        CovidPatientHistoryViewHolder holder = PowerMockito.mock(CovidPatientHistoryViewHolder.class);
+        TextView keyTextView = PowerMockito.mock(TextView.class);
+        TextView valTextView = PowerMockito.mock(TextView.class);
 
-        when(holder.getTvHistoryKey()).thenReturn(keyTextView);
-        when(holder.getTvHistoryValue()).thenReturn(valTextView);
+        PowerMockito.when(holder.getTvHistoryKey()).thenReturn(keyTextView);
+        PowerMockito.when(holder.getTvHistoryValue()).thenReturn(valTextView);
 
         adapter.onBindViewHolder(holder, 0);
 
-        verify(keyTextView, Mockito.times(1)).setText("patient_name");
-        verify(valTextView, Mockito.times(1)).setText("Jane");
+        Mockito.verify(keyTextView, Mockito.times(1)).setText("patient_name");
+        Mockito.verify(valTextView, Mockito.times(1)).setText("Jane");
     }
 
     @Test
     public void getItemCountShouldReturnOne() {
-        assertEquals(1, list.size());
+        Assert.assertEquals(1, list.size());
     }
 }
