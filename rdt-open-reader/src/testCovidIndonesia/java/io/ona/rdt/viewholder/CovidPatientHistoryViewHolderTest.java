299141f855bb404bd993a91097d09799f06046a3
syedowaisali <12345>
2020-09-02 21:29:36 +0500

    :heavy_check_mark: added test case

---
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/viewholder/CovidPatientHistoryViewHolderTest.java
@@ -0,0 +1,56 @@
+package io.ona.rdt.viewholder;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.ona.rdt.R;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CovidPatientHistoryViewHolderTest {
+
+    private CovidPatientHistoryViewHolder covidPatientHistoryViewHolder;
+
+    @Mock
+    private TextView tvHistoryKey;
+
+    @Mock
+    private TextView tvHistoryVal;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        View itemView = Mockito.mock(View.class);
+
+        Mockito.doReturn(tvHistoryKey).when(itemView).findViewById(Mockito.eq(R.id.tv_history_key));
+        Mockito.doReturn(tvHistoryVal).when(itemView).findViewById(Mockito.eq(R.id.tv_history_value));
+
+        covidPatientHistoryViewHolder = new CovidPatientHistoryViewHolder(itemView);
+    }
+
+    @After
+    public void tearDown() {
+        covidPatientHistoryViewHolder = null;
+    }
+
+    @Test
+    public void testGetTvHistoryKeyShouldReturnValidTextView() {
+        Assert.assertEquals(tvHistoryKey, covidPatientHistoryViewHolder.getTvHistoryKey());
+    }
+
+    @Test
+    public void testGetTvHistoryValueShouldReturnValidTextView() {
+        Assert.assertEquals(tvHistoryVal, covidPatientHistoryViewHolder.getTvHistoryValue());
+    }
+}
