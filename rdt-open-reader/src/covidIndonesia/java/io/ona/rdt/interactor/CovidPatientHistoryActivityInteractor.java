53e05de02761d9d531dbc06aad431b097fefe853
Vincent Karuri <vincentmkaruri@gmail.com>
2020-09-04 18:42:54 +0300

    Remove inaccurate repeating group processing logic

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/interactor/CovidPatientHistoryActivityInteractor.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/interactor/CovidPatientHistoryActivityInteractor.java
@@ -1,7 +1,6 @@
 package io.ona.rdt.interactor;
 
 import org.apache.commons.lang3.StringUtils;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.smartregister.domain.Event;
 import org.smartregister.domain.Obs;
@@ -20,7 +19,6 @@ import io.ona.rdt.repository.PatientHistoryRepository;
 import io.ona.rdt.util.Constants;
 import io.ona.rdt.util.CovidConstants;
 import io.ona.rdt.util.FormKeyTextExtractionUtil;
-import timber.log.Timber;
 
 /**
  * Created by Vincent Karuri on 24/08/2020
@@ -80,16 +78,6 @@ public class CovidPatientHistoryActivityInteractor {
         return StringUtils.join(values, ", ");
     }
 
-    public JSONArray getJSONArr(String str) {
-        JSONArray jsonArray = null;
-        try {
-            jsonArray = new JSONArray(str);
-        } catch (JSONException ex) {
-            Timber.i("Invalid JSON array obs value.");
-        }
-        return jsonArray;
-    }
-
     /**
      * Takes care of the special case in native radio button and checkbox where the key should be used
      *
@@ -98,11 +86,6 @@ public class CovidPatientHistoryActivityInteractor {
      * @return
      */
     private String getValue(String value, Map<String, String> formWidgetKeyToTextMap) {
-        // if value is a json string, return the length of json array (occurs in repeating groups)
-        JSONArray jsonArray = getJSONArr(value);
-        if (jsonArray != null) {
-            return String.valueOf(jsonArray.length());
-        }
         return formWidgetKeyToTextMap.containsKey(value) ? formWidgetKeyToTextMap.get(value) : value;
     }
 
