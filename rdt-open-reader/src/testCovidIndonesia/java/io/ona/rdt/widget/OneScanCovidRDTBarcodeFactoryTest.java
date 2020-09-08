4e24552dacfa51039bd4eab9e9040053b46e43a3
syedowaisali <12345>
2020-09-08 18:02:46 +0500

    :hammer: remove static import

--- rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/widget/OneScanCovidRDTBarcodeFactoryTest.java
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/widget/OneScanCovidRDTBarcodeFactoryTest.java
@@ -1,13 +1,9 @@
 package io.ona.rdt.widget;
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.widget.RelativeLayout;
 
-import com.rengwuxian.materialedittext.MaterialEditText;
-import com.rey.material.widget.Button;
 import com.vijay.jsonwizard.activities.JsonFormActivity;
 import com.vijay.jsonwizard.domain.WidgetArgs;
 
@@ -17,6 +13,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -27,23 +24,16 @@ import org.powermock.reflect.Whitebox;
 
 import java.util.Date;
 
-import io.ona.rdt.R;
 import io.ona.rdt.application.RDTApplication;
 import io.ona.rdt.fragment.RDTJsonFormFragment;
 import io.ona.rdt.util.Constants;
 import io.ona.rdt.util.StepStateConfig;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.powermock.api.mockito.PowerMockito.mockStatic;
-
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({RDTJsonFormFragment.class, Bundle.class, RDTApplication.class, LayoutInflater.class})
 public class OneScanCovidRDTBarcodeFactoryTest {
+
+    private static final String STEP_6 = "step6";
 
     private OneScanCovidRDTBarcodeFactory oneScanCovidRDTBarcodeFactory;
 
@@ -60,7 +50,6 @@ public class OneScanCovidRDTBarcodeFactoryTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         oneScanCovidRDTBarcodeFactory = new OneScanCovidRDTBarcodeFactory();
-        //mockStaticClasses();
         mockStaticMethods();
         setWidgetArgs();
     }
@@ -72,14 +61,14 @@ public class OneScanCovidRDTBarcodeFactoryTest {
 
     @Test
     public void testMoveToNextStepShouldGotoTwoSensorTriggered() {
-        oneScanCovidRDTBarcodeFactory.moveToNextStep(mock(Date.class));
+        oneScanCovidRDTBarcodeFactory.moveToNextStep(PowerMockito.mock(Date.class));
     }
 
     private void setWidgetArgs() throws JSONException {
         widgetArgs = new WidgetArgs();
-        RDTJsonFormFragment formFragment = mock(RDTJsonFormFragment.class);
+        RDTJsonFormFragment formFragment = PowerMockito.mock(RDTJsonFormFragment.class);
         widgetArgs.setFormFragment(formFragment);
-        widgetArgs.setStepName("step6");
+        widgetArgs.setStepName(STEP_6);
 
         String step = "{\n" +
                 "    \"title\": \"Sample collection\",\n" +
@@ -103,37 +92,26 @@ public class OneScanCovidRDTBarcodeFactoryTest {
 
         widgetArgs.setJsonObject(jsonObject);
 
-        jsonFormActivity = mock(JsonFormActivity.class);
-        Resources resources = mock(Resources.class);
-        doReturn(resources).when(jsonFormActivity).getResources();
-        doReturn(0).when(resources).getColor(anyInt());
-        doReturn(jsonObject).when(jsonFormActivity).getStep(Mockito.eq("step6"));
+        jsonFormActivity = PowerMockito.mock(JsonFormActivity.class);
+        Resources resources = PowerMockito.mock(Resources.class);
+        PowerMockito.doReturn(resources).when(jsonFormActivity).getResources();
+        PowerMockito.doReturn(0).when(resources).getColor(ArgumentMatchers.anyInt());
+        PowerMockito.doReturn(jsonObject).when(jsonFormActivity).getStep(Mockito.eq(STEP_6));
         widgetArgs.setContext(jsonFormActivity);
         Whitebox.setInternalState(oneScanCovidRDTBarcodeFactory, "widgetArgs", widgetArgs);
     }
 
-    private void mockStaticMethods() throws JSONException {
+    private void mockStaticMethods() {
 
-        mockStatic(RDTApplication.class);
+        PowerMockito.mockStatic(RDTApplication.class);
         PowerMockito.when(RDTApplication.getInstance()).thenReturn(rdtApplication);
         PowerMockito.when(rdtApplication.getStepStateConfiguration()).thenReturn(stepStateConfig);
 
-        JSONObject jsonObject = mock(JSONObject.class);
-        doReturn("step11").when(jsonObject).optString(eq(Constants.Step.SENSOR_TRIGGERED_PAGE), Mockito.eq("step1"));
+        JSONObject jsonObject = PowerMockito.mock(JSONObject.class);
+        PowerMockito.doReturn("step11").when(jsonObject).optString(Mockito.eq(Constants.Step.SENSOR_TRIGGERED_PAGE), Mockito.eq("step1"));
 
-        doReturn(jsonObject).when(stepStateConfig).getStepStateObj();
+        PowerMockito.doReturn(jsonObject).when(stepStateConfig).getStepStateObj();
 
         Whitebox.setInternalState(oneScanCovidRDTBarcodeFactory, "stepStateConfig", stepStateConfig);
-    }
-
-    private void mockStaticClasses() {
-        mockStatic(LayoutInflater.class);
-        LayoutInflater layoutInflater = mock(LayoutInflater.class);
-        mockStatic(LayoutInflater.class);
-        RelativeLayout rootLayout = mock(RelativeLayout.class);
-        PowerMockito.when(LayoutInflater.from(any(Context.class))).thenReturn(layoutInflater);
-        doReturn(rootLayout).when(layoutInflater).inflate(anyInt(), isNull());
-        doReturn(mock(MaterialEditText.class)).when(rootLayout).findViewById(R.id.edit_text);
-        doReturn(mock(Button.class)).when(rootLayout).findViewById(R.id.scan_button);
     }
 }
