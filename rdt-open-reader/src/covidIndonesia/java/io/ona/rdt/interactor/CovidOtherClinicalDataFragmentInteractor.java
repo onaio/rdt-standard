8efb6a80045dd0f68e07a300ef8ae708b2c47e97
Vincent Karuri <vincentmkaruri@gmail.com>
2020-09-10 18:50:25 +0300

    Clean up redundant code

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/interactor/CovidOtherClinicalDataFragmentInteractor.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/interactor/CovidOtherClinicalDataFragmentInteractor.java
@@ -1,9 +1,9 @@
 package io.ona.rdt.interactor;
 
-import io.ona.rdt.util.CovidFormLauncherAndSaver;
+import io.ona.rdt.util.CovidFormLauncher;
 
 /**
  * Created by Vincent Karuri on 10/09/2020
  */
-public class CovidOtherClinicalDataFragmentInteractor extends CovidFormLauncherAndSaver {
+public class CovidOtherClinicalDataFragmentInteractor extends CovidFormLauncher {
 }
