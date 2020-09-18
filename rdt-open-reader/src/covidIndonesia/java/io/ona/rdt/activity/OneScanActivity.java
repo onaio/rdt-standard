47e0e43338cc51b516d27511ce280c7ffd2e0f4b
Vincent Karuri <vincentmkaruri@gmail.com>
2020-09-18 11:37:15 +0300

    Populate barcode details on results screen

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/activity/OneScanActivity.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/activity/OneScanActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.vision.barcode.Barcode;
 
@@ -59,22 +60,30 @@ public class OneScanActivity extends AppCompatActivity implements View.OnClickLi
                 response = new OneScanHelper.ScanResponse(bundle);
                 try {
                     displayStatusMessageAndSymbol(response.sensorTriggered, response.expirationDate);
-//                resultView.setText(String.format("\tStatus: %s\n\n\tBarcode Text: %s" +
-//                                "\n\n\tProduct ID: %s\n\n\tSerial No: %s\n\n\tAdditional ID: %s\n\n" +
-//                                "\tLot: %s\n\n\tExpiration Date: %s\n\n\tSensor triggered: %s",
-//                        response.status,
-//                        response.barcodeText,
-//                        response.productId,
-//                        response.serialNumber,
-//                        response.additionalIdentifier,
-//                        response.lot,
-//                        response.expirationDate,
-//                        response.sensorTriggered ? "yes" : "no"));
+                    populateBarcodeDetails();
                 } catch (ParseException e) {
                     Timber.e(e);
                 }
             }
         });
+    }
+
+    private void populateBarcodeDetails() {
+        setBarcodeResult(R.id.barcode_product_id, response.productId);
+        setBarcodeResult(R.id.barcode_serial_no, response.serialNumber);
+        setBarcodeResult(R.id.barcode_additional_id, response.additionalIdentifier);
+        setBarcodeResult(R.id.barcode_lot_no, response.lot);
+        setBarcodeResult(R.id.barcode_expiration_date, response.expirationDate);
+        setBarcodeResult(R.id.barcode_is_sensor_triggered, response.sensorTriggered ? "yes" : "no");
+    }
+
+    private void setBarcodeResult(int viewId, String result) {
+        View barcodeResultRow = findViewById(viewId);
+        if (StringUtils.isBlank(result)) {
+            barcodeResultRow.setVisibility(View.GONE);
+        } else {
+            ((TextView) barcodeResultRow.findViewById(R.id.tv_barcode_result_value)).setText(result);
+        }
     }
 
     private void displayStatusMessageAndSymbol(boolean isSensorTriggered, String expirationDate) throws ParseException {
