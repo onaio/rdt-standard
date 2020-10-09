fabfa88dc7426311e1f576e51c899d9d2072da43
Vincent Karuri <vincentmkaruri@gmail.com>
2020-10-09 13:35:45 +0300

    Add queries to extract device and manufacturer names

--- rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/util/FHIRResourceProcessorTest.java
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/util/FHIRResourceProcessorTest.java
@@ -3,6 +3,7 @@ package io.ona.rdt.util;
 import com.ibm.fhir.model.format.Format;
 import com.ibm.fhir.model.parser.FHIRParser;
 import com.ibm.fhir.model.resource.Bundle;
+import com.ibm.fhir.model.resource.DeviceDefinition;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,15 +23,21 @@ public class FHIRResourceProcessorTest extends RobolectricTest {
     public void testFHIRQueriesOnDeviceDefinitionResource() throws Exception {
         PathEvaluatorLibrary.init(null, null, null, null);
         InputStream stream = RuntimeEnvironment.application.getAssets().open("DeviceDefinition.json");
-        Bundle domainResource = FHIRParser.parser(Format.JSON).parse(stream);
+        Bundle deviceDefinitionBundle = FHIRParser.parser(Format.JSON).parse(stream);
 
         // extract instructions
         String expression = "$this.entry.resource.where(identifier.where(value='d3fdac0e-061e-b068-2bed-5a95e803636f')).capability.where(type.where(text='instructions')).description.text";
-        Assert.assertEquals("Collect blood sample",
-                PathEvaluatorLibrary
-                        .getInstance()
-                        .evaluateElementExpression(domainResource,
-                                expression)
-                        .getValue().asStringValue().string());
+        Assert.assertEquals("Collect blood sample", PathEvaluatorLibrary.getInstance().extractStringFromBundle(deviceDefinitionBundle, expression));
+
+        // extract device id
+
+        // extract device name
+        expression = "$this.entry.resource.where(identifier.where(value='d3fdac0e-061e-b068-2bed-5a95e803636f')).deviceName.name";
+        Assert.assertEquals("Wondfo SARS-CoV-2 Antibody Test", PathEvaluatorLibrary.getInstance().extractStringFromBundle(deviceDefinitionBundle, expression));
+
+        // extract manufacturer name
+        expression = "$this.entry.resource.where(identifier.where(value='d3fdac0e-061e-b068-2bed-5a95e803636f')))";
+        DeviceDefinition deviceDefinition =  (DeviceDefinition) PathEvaluatorLibrary.getInstance().extractResourceFromBundle(deviceDefinitionBundle, expression);
+        Assert.assertEquals("Guangzhou Wondfo Biotech", deviceDefinition.getManufacturer().as(com.ibm.fhir.model.type.String.class).getValue());
     }
 }
