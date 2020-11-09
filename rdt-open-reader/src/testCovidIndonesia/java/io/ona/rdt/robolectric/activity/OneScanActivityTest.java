8679b4bafb9b75db2d509f38170065f5f05233d5
zeeshan <zeeshan.afzal@venturedive.com>
2020-11-09 20:01:04 +0500

    unit tests for CovidRDTBarcodeFactory and OneScanActivity

--- rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/robolectric/activity/OneScanActivityTest.java
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/robolectric/activity/OneScanActivityTest.java
@@ -1,21 +1,36 @@
 package io.ona.rdt.robolectric.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.barcode.Barcode;
+
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.Robolectric;
+import org.robolectric.Shadows;
+import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.util.ReflectionHelpers;
+
+import java.io.Serializable;
+import java.sql.Ref;
 
 import io.ona.rdt.R;
 import io.ona.rdt.TestUtils;
 import io.ona.rdt.activity.OneScanActivity;
+import io.ona.rdt.util.OneScanHelper;
 import io.ona.rdt.widget.CovidRDTBarcodeFactory;
+
+import static android.app.Activity.RESULT_OK;
+import static com.vijay.jsonwizard.constants.JsonFormConstants.BARCODE_CONSTANTS.BARCODE_KEY;
 
 /**
  * Created by Vincent Karuri on 18/09/2020
@@ -25,7 +40,7 @@ public class OneScanActivityTest extends ActivityRobolectricTest {
     private OneScanActivity oneScanActivity;
 
     @Before
-    public void setUp()  {
+    public void setUp() {
         oneScanActivity = Robolectric.buildActivity(OneScanActivity.class).create().resume().get();
     }
 
@@ -36,7 +51,7 @@ public class OneScanActivityTest extends ActivityRobolectricTest {
         final String productId = "product_id";
         final String lot = "lot_no";
         final String serialNumber = "serial_no";
-        final String additionalIdentifier =  "identifier";
+        final String additionalIdentifier = "identifier";
         final String expirationDate = TestUtils.getFormattedDateWithOffset(-1, CovidRDTBarcodeFactory.RDT_BARCODE_EXPIRATION_DATE_FORMAT);
         final String sensorNotTriggered = "No";
         final String sensorTriggeredStr = "sensorTriggered";
@@ -87,7 +102,7 @@ public class OneScanActivityTest extends ActivityRobolectricTest {
         verifyBarcodeResult(R.id.barcode_additional_id, additionalIdentifier);
         verifyBarcodeResult(R.id.barcode_lot_no, lot);
         verifyBarcodeResult(R.id.barcode_expiration_date, expirationDate);
-        verifyBarcodeResult(R.id.barcode_is_sensor_triggered,   sensorNotTriggered);
+        verifyBarcodeResult(R.id.barcode_is_sensor_triggered, sensorNotTriggered);
 
         // verify details for missing information
         bundle = new Bundle();
@@ -119,7 +134,7 @@ public class OneScanActivityTest extends ActivityRobolectricTest {
         final String productId = "product_id";
         final String lot = "lot_no";
         final String serialNumber = "serial_no";
-        final String additionalIdentifier =  "identifier";
+        final String additionalIdentifier = "identifier";
         final String expirationDate = TestUtils.getFormattedDateWithOffset(-1, CovidRDTBarcodeFactory.RDT_BARCODE_EXPIRATION_DATE_FORMAT);
         final String sensorNotTriggered = "No";
         final String sensorTriggeredStr = "sensorTriggered";
@@ -151,6 +166,47 @@ public class OneScanActivityTest extends ActivityRobolectricTest {
         JSONArray dataArray = ReflectionHelpers.getField(oneScanActivity, dataArrayField);
 
         Assert.assertEquals(2, dataArray.length());
+
+    }
+
+    @Test
+    public void testActivityResultShouldVerifyCorrectResultData() throws JSONException {
+        final String setResultAndFinishMethod = "setResultAndFinish";
+        final String enableBatchScanField = "enableBatchScan";
+        Bundle bundle = new Bundle();
+        bundle.putString("status", "ok");
+        bundle.putString("barcodeText", "test");
+        bundle.putBoolean("sensorTriggered", false);
+        bundle.putString("productId", "test-product");
+        bundle.putString("lot", "test-lot");
+        bundle.putString("expirationDate", TestUtils.getFormattedDateWithOffset(1, CovidRDTBarcodeFactory.RDT_BARCODE_EXPIRATION_DATE_FORMAT));
+        bundle.putString("serialNumber", "test-serial");
+        bundle.putString("additionalIdentifier", "test-additional-identifier");
+        final OneScanHelper.ScanResponse scanResponse = new OneScanHelper.ScanResponse(bundle);
+
+        ShadowActivity shadowOneScanActivity = Shadows.shadowOf(oneScanActivity);
+        ReflectionHelpers.setField(oneScanActivity, enableBatchScanField, true);
+        ReflectionHelpers.callInstanceMethod(oneScanActivity, setResultAndFinishMethod,
+                ReflectionHelpers.ClassParameter.from(OneScanHelper.ScanResponse.class, scanResponse));
+
+        String resultData = shadowOneScanActivity.getResultIntent().getStringExtra("data");
+        Assert.assertEquals(RESULT_OK, shadowOneScanActivity.getResultCode());
+        Assert.assertNotNull(resultData);
+        JSONObject resultObj = new JSONObject(resultData);
+        Assert.assertTrue(resultObj.has("scans"));
+
+        ReflectionHelpers.setField(oneScanActivity, enableBatchScanField, false);
+        ReflectionHelpers.callInstanceMethod(oneScanActivity, setResultAndFinishMethod,
+                ReflectionHelpers.ClassParameter.from(OneScanHelper.ScanResponse.class, scanResponse));
+
+        Barcode resultBarcode = shadowOneScanActivity.getResultIntent().getParcelableExtra(BARCODE_KEY);
+        final String displayValues = StringUtils.join(new String[]{scanResponse.serialNumber, scanResponse.expirationDate,
+                scanResponse.lot, scanResponse.productId, String.valueOf(scanResponse.sensorTriggered),
+                scanResponse.status}, ',');
+
+        Assert.assertEquals(RESULT_OK, shadowOneScanActivity.getResultCode());
+        Assert.assertNotNull(resultBarcode);
+        Assert.assertEquals(displayValues, resultBarcode.displayValue);
 
     }
 
