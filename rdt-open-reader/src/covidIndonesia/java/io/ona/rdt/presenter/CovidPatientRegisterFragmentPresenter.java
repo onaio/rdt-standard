1094d3a20d68e3e23459492bfc00988b567f24a9
Vincent Karuri <vincentmkaruri@gmail.com>
2020-11-18 12:51:05 +0300

    Fix main column bug

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/presenter/CovidPatientRegisterFragmentPresenter.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/presenter/CovidPatientRegisterFragmentPresenter.java
@@ -4,6 +4,13 @@ import io.ona.rdt.contract.PatientRegisterFragmentContract;
 import io.ona.rdt.interactor.CovidPatientRegisterFragmentInteractor;
 import io.ona.rdt.interactor.PatientRegisterFragmentInteractor;
 
+import static io.ona.rdt.util.Constants.DBConstants.AGE;
+import static io.ona.rdt.util.Constants.DBConstants.DOB;
+import static io.ona.rdt.util.Constants.DBConstants.FIRST_NAME;
+import static io.ona.rdt.util.Constants.DBConstants.LAST_NAME;
+import static io.ona.rdt.util.Constants.DBConstants.PATIENT_ID;
+import static io.ona.rdt.util.Constants.DBConstants.SEX;
+
 /**
  * Created by Vincent Karuri on 05/08/2020
  */
@@ -13,7 +20,13 @@ public class CovidPatientRegisterFragmentPresenter extends PatientRegisterFragme
         super(patientRegisterFragment);
     }
 
+    @Override
     protected PatientRegisterFragmentInteractor createInteractor() {
         return new CovidPatientRegisterFragmentInteractor();
+    }
+
+    @Override
+    protected String[] getMainColumns() {
+        return new String[] {"relationalid", FIRST_NAME, LAST_NAME, AGE, SEX, PATIENT_ID, DOB};
     }
 }
