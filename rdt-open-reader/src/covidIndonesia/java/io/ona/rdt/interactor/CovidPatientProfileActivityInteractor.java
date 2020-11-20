3ebc52cac76b280ad4ab8d4f169183ee6cd3ed49
Vincent Karuri <vincentmkaruri@gmail.com>
2020-11-20 16:54:55 +0300

    Make CovidPatientProfileActivityInteractor a CovidFormSaver to correctly process covid events

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/interactor/CovidPatientProfileActivityInteractor.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/interactor/CovidPatientProfileActivityInteractor.java
@@ -1,7 +1,9 @@
 package io.ona.rdt.interactor;
 
+import io.ona.rdt.util.CovidFormSaver;
+
 /**
  * Created by Vincent Karuri on 17/02/
  */
-public class CovidPatientProfileActivityInteractor extends PatientProfileActivityInteractor {
+public class CovidPatientProfileActivityInteractor extends CovidFormSaver {
 }
