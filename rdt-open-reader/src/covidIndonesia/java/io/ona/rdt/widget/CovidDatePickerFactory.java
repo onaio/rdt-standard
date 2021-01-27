f5400e576fe4191e0485ac04b2627294be3347ee
syedowaisali <owais.ali@venturedive.com>
2021-01-27 16:28:43 +0500

    :hammer: WIP

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/widget/CovidDatePickerFactory.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/widget/CovidDatePickerFactory.java
@@ -29,8 +29,8 @@ public class CovidDatePickerFactory extends DatePickerFactory {
 
         super.attachLayout(stepName, context, formFragment, jsonObject, editText, duration);
         try {
-            JSONObject maxAllowDateObj = jsonObject.getJSONObject(V_MIN_ALLOWED_DATE);
-            if (maxAllowDateObj != null) {
+            if (jsonObject.has(V_MIN_ALLOWED_DATE)) {
+                JSONObject maxAllowDateObj = jsonObject.getJSONObject(V_MIN_ALLOWED_DATE);
                 editText.setId(R.id.edit_text);
                 editText.addValidator(new MinAllowedDateValidator(maxAllowDateObj.getString(JsonFormConstants.ERR),
                         maxAllowDateObj.getString(JsonFormConstants.VALUE)));
