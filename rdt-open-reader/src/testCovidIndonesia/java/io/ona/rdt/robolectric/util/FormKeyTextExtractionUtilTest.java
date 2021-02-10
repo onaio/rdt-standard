b49c32dd34e503c0c7192f15d56d8d5207c44cba
syedowaisali90 <owais.ali@venturedive.com>
2021-02-11 02:14:56 +0500

    :hammer: enable flag print log

--- rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/robolectric/util/FormKeyTextExtractionUtilTest.java
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/robolectric/util/FormKeyTextExtractionUtilTest.java
@@ -29,6 +29,13 @@ public class FormKeyTextExtractionUtilTest extends RobolectricTest {
         for (Map.Entry<String, String> entry : expectedData.entrySet()) {
             String expectedValue = entry.getValue();
             String actualValue = actualData.get(entry.getKey());
+            System.out.println("expected key: " + entry.getKey());
+            System.out.println("expected val: " + expectedValue + " - actual val: " + actualValue);
+        }
+
+        for (Map.Entry<String, String> entry : expectedData.entrySet()) {
+            String expectedValue = entry.getValue();
+            String actualValue = actualData.get(entry.getKey());
             System.out.println("expected: " + expectedValue + " - actual: " + actualValue);
             Assert.assertEquals(expectedValue, actualValue);
         }
