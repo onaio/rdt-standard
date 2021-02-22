93c6618add4b4e9038b79e9c6a8a0a3cbca55b1c
syedowaisali90 <owais.ali@venturedive.com>
2021-02-22 19:38:39 +0500

    :hammer: fixed codacy issue

--- rdt-open-reader/src/test/java/io/ona/rdt/domain/PatientTest.java
+++ rdt-open-reader/src/test/java/io/ona/rdt/domain/PatientTest.java
@@ -26,6 +26,8 @@ public class PatientTest {
 
     @Test
     public void testSetters() {
+        int age = 10;
+
         assertEquals("name", patient.getPatientName());
         assertEquals("sex", patient.getPatientSex());
         assertEquals("entity_id", patient.getBaseEntityId());
@@ -34,14 +36,14 @@ public class PatientTest {
         patient.setPatientSex("sex_2");
         patient.setPatientName("name_2");
         patient.setPatientId("patient_id");
-        patient.setAge(10);
+        patient.setAge(age);
         patient.setDob("1989-11-08");
         patient.setFemaleTranslatedSex("female");
         assertEquals("name_2", patient.getPatientName());
         assertEquals("sex_2", patient.getPatientSex());
         assertEquals("entity_id_2", patient.getBaseEntityId());
         assertEquals("patient_id", patient.getPatientId());
-        assertEquals(10, patient.getAge());
+        assertEquals(age, patient.getAge());
         assertEquals("1989-11-08", patient.getDob());
         assertEquals("female", patient.getFemaleTranslatedSex());
     }
