8543f73d34efc91e9e0e090bd61f45dd2c607bbd
syedowaisali90 <owais.ali@venturedive.com>
2021-03-26 16:10:14 +0500

    update test case

--- rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/robolectric/activity/CovidPatientRegisterActivityTest.java
+++ rdt-open-reader/src/testCovidIndonesia/java/io/ona/rdt/robolectric/activity/CovidPatientRegisterActivityTest.java
@@ -35,7 +35,6 @@ public class CovidPatientRegisterActivityTest extends ActivityRobolectricTest {
         MockitoAnnotations.initMocks(this);
         covidPatientRegisterActivity = Robolectric.buildActivity(CovidPatientRegisterActivity.class)
                 .create()
-                .resume()
                 .get();
     }
 
