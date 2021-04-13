a259de96955632e270821820d50c2cf2bf5e3465
syedowaisali90 <owais.ali@venturedive.com>
2021-04-13 15:08:50 +0500

    fixed location cancelation crash

--- rdt-open-reader/src/main/java/io/ona/rdt/activity/PatientRegisterActivity.java
+++ rdt-open-reader/src/main/java/io/ona/rdt/activity/PatientRegisterActivity.java
@@ -190,9 +190,6 @@ public class PatientRegisterActivity extends BaseRegisterActivity implements Syn
             // for exceptions thrown and caught during widget instantiation
             Bundle bundle = data.getExtras();
             throw  (RuntimeException) bundle.getSerializable(JsonFormConstants.RESULT_INTENT.RUNTIME_EXCEPTION);
-        } else if (requestCode == REQUEST_CODE_GET_JSON && resultCode == RESULT_CANCELED) {
-            // for general uncaught runtime exceptions thrown by json form activity
-            throw new RuntimeException("Runtime exception thrown by json form activity!");
         } else if (requestCode == REQUEST_CODE_GET_JSON && resultCode == RESULT_OK && data != null) {
             String jsonForm = data.getStringExtra("json");
             Timber.d(jsonForm);
