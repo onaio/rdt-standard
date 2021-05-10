9e60a720d5adb2563dacdb860e4884f43e22f848
syedowaisali90 <owais.ali@venturedive.com>
2021-05-10 14:26:51 +0500

    fixed crashing issue

--- rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/widget/CovidEditTextFactory.java
+++ rdt-open-reader/src/covidIndonesia/java/io/ona/rdt/widget/CovidEditTextFactory.java
@@ -29,7 +29,13 @@ public class CovidEditTextFactory extends EditTextFactory {
 
     @Override
     protected void attachLayout(String stepName, Context context, JsonFormFragment formFragment, JSONObject jsonObject, MaterialEditText editText, ImageView editButton) throws Exception {
-        super.attachLayout(stepName, context, formFragment, jsonObject, editText, editButton);
+        formFragment.getJsonApi().getAppExecutors().mainThread().execute(() -> {
+            try {
+                CovidEditTextFactory.super.attachLayout(stepName, context, formFragment, jsonObject, editText, editButton);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
         if (!jsonObject.has(JsonFormConstants.HINT)) {
             editText.setFloatingLabel(MaterialEditText.FLOATING_LABEL_NONE);
